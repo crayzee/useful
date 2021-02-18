542b7b2e3ba2004259333ad70d56ba472239bf87
crayzee <twopik@gmail.com>
2021-02-18 21:35:20 +0300

    2 new routes: /all and /delete/{user_id}

--- src/app/user/api.py
+++ src/app/user/api.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
-from tortoise.contrib.fastapi import HTTPNotFoundError
+from fastapi import APIRouter, Depends
 
 from src.app.auth.permissions import get_user
 
@@ -15,23 +14,3 @@ def user_me(current_user: models.User = Depends(get_user)):
     """
     if current_user:
         return current_user
-
-
-@user_router.delete('/delete/{user_id}', response_model=schemas.UserStatus, responses={404: {"model": HTTPNotFoundError}})
-async def delete_user(user_id: int):
-    """ Delete user
-    """
-    deleted_count = await service.user_s.delete(id=user_id)
-    if not deleted_count:
-        raise HTTPException(status_code=404, detail=f"User with id={user_id} not found")
-    return schemas.UserStatus(message=f"Deleted user {user_id}")
-
-
-@user_router.get('/all', response_model=schemas.UserPublicList)
-async def user_list():
-    """ Get user
-    """
-    users = await service.user_s.get_all()
-    return {
-        "users": users
-    }

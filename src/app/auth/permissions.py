17cd810a91e9310e0d7409a68bf35276f00555a5
crayzee <twopik@gmail.com>
2021-02-18 21:28:17 +0300

    update get_current_user for async use

--- src/app/auth/permissions.py
+++ src/app/auth/permissions.py
@@ -16,7 +16,7 @@ from .schemas import TokenPayload
 reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/api/v1/login/access-token")
 
 
-def get_current_user(token: str = Security(reusable_oauth2)):
+async def get_current_user(token: str = Security(reusable_oauth2)):
     """ Check OAuth2 user
     """
     try:
@@ -26,7 +26,7 @@ def get_current_user(token: str = Security(reusable_oauth2)):
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
         )
-    user = service.user_s.get_obj(id=token_data.user_id)
+    user = await service.user_s.get_obj(id=token_data.user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
@@ -46,3 +46,4 @@ def get_superuser(current_user: User = Security(get_current_user)):
             status_code=400, detail="The user doesn't have enough privileges"
         )
     return current_user
+

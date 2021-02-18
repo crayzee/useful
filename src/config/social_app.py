feb66f276139bd9d074b770f64a8353500aa12c9
crayzee <twopik@gmail.com>
2021-02-18 21:30:11 +0300

    added timeout 13 s

--- src/config/social_app.py
+++ src/config/social_app.py
@@ -1,5 +1,5 @@
 from authlib.integrations.starlette_client import OAuth
-
+from httpx import Timeout
 
 social_oauth = OAuth()
 
@@ -10,9 +10,10 @@ social_oauth.register(
     client_id='99118809b9098fa3fa00',
     client_secret='a5899c5cb8ddc411a241880d0ebf362a3098cb8a',
     access_token_url='https://github.com/login/oauth/access_token',
-    access_token_params = None,
+    access_token_params=None,
     authorize_url='https://github.com/login/oauth/authorize',
     authorize_params=None,
+
     api_base_url='https://api.github.com/',
-    client_kwargs={'scope': 'user:email'},
+    client_kwargs={'scope': 'user:email', 'timeout': Timeout(timeout=13)},
 )

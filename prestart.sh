7301722f2a66224ad46d54905f1f28dc6addcf14
crayzee <twopik@gmail.com>
2021-02-21 22:38:24 +0300

    change log

--- prestart.sh
+++ prestart.sh
@@ -1,10 +1,8 @@
-#! /usr/bin/env bash
-
 # Let the DB start
 #sleep 10;
 #
 #poetry run alembic revision --autogenerate
-
+#
 #sleep 10;
 # Run migrations
 #poetry run alembic upgrade head
--- src/app/routers.py
+++ src/app/routers.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 from src.app.auth.api import auth_router
-#from src.app.blog.api import blog_router
 from src.app.user.api import user_router
+from src.app.board.endpoint import category, toolkit, project, task
 
 api_router = APIRouter()
 
-#api_router.include_router(blog_router, prefix="/blog", tags=["blog"])
 api_router.include_router(auth_router, prefix='/auth', tags=["login"])
 api_router.include_router(user_router, prefix='/user', tags=["user"])
+api_router.include_router(category.category_router, prefix='/board/category', tags=["board"])
+api_router.include_router(toolkit.toolkit_router, prefix='/board/toolkit', tags=["board"])
+api_router.include_router(project.project_router, prefix='/board/project', tags=["board"])
+api_router.include_router(task.task_router, prefix='/board/task', tags=["board"])
